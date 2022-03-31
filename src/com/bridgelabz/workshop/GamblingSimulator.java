bf6096e2357b0f2d88a930fecf774e7a0d3723b7
Kishlay Kishan <kishan.kishlay20@gmail.com>
2022-03-31 08:58:10 +0530

    Added:check Winner or Looser

--- src/com/bridgelabz/workshop/GamblingSimulator.java
+++ src/com/bridgelabz/workshop/GamblingSimulator.java
@@ -1,16 +1,34 @@
 package com.bridgelabz.workshop;
 
+import java.util.Random;
+
 public class GamblingSimulator {
 
-	public static void main(String[] args) {
-		int STAKE = 100;
-		int BET =  1;
-		System.out.println("Welcome To Gambling Simulator");
-		System.out.println("Gambler start with stake of $" + STAKE + " every day");
-		System.out.println("Gambler bet for $" + BET + " every game");
+	
+		public static final int STAKE_PER_DAY = 100;
+		public static final int BET_PER_GAME = 1;
+		public static final int WIN = 1;
+		public static int stake = 0;
+
+		
+		public static void winCheck() {
+			Random random = new Random();
+			int betReturns = random.nextInt(2);
+			if (betReturns == WIN) {
+				stake++;// incrementing
+				System.out.println("win");
+			} else {
+				stake--;// decrementing
+				System.out.println("loss");
+			}
+			System.out.println("stake = " + stake);
+
+		}
+
+		public static void main(String[] args) {
+			System.out.println("Welcome to Gambling Simulation");
+			winCheck();// calling function
+		}
+
 	}
-
-
-}
-
 
